345d05e738f089f296a26f1821ab9a7f19d40282
Aakib khan <101191261+Aakibgithuber@users.noreply.github.com>
2025-03-10 10:52:49 +0530

    Update cdk.ts

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -1,12 +1,4 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import * as projectConfig from '../config/projectConfig.json';
 
 const app = new cdk.App();
-
-new KaitoApplicationStack(app, 'KaitoApplicationStack', {
-  stackName: `${projectConfig.projectPrefix}-pipeline-stack`,
-  env: {
-    region: projectConfig.environments.DEV.region,
-  },
-});
