b4824ce11036aa5bf9ce24f3909a4ea28e0223de
Aakib khan <101191261+Aakibgithuber@users.noreply.github.com>
2025-03-10 10:54:42 +0530

    Update cdk.ts

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
+import { KaitoApplicationStack } from '../lib/deploy-stack'; // Ensure this path is correct
 
 const app = new cdk.App();
+new KaitoApplicationStack(app, 'KaitoApplicationStack'); // Add this back
