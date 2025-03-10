cd418ce44e5266561b609d484f7595218fd84485
Aakib khan <101191261+Aakibgithuber@users.noreply.github.com>
2025-03-10 12:51:30 +0530

    Update deploy-stack.ts

--- lib/deploy-stack.ts
+++ lib/deploy-stack.ts
@@ -13,34 +13,8 @@ export class MyAwsSetupStack extends cdk.Stack {
       versioned: true,
       removalPolicy: cdk.RemovalPolicy.DESTROY
     });
-
-    // Step 2: Create an Elastic Beanstalk Application
-    const ebApp = new elasticbeanstalk.CfnApplication(this, 'MyElasticBeanstalkApp', {
-      applicationName: 'MyApp'
-    });
-
-    // Step 3: Create an Elastic Beanstalk Environment
-    const ebEnv = new elasticbeanstalk.CfnEnvironment(this, 'MyElasticBeanstalkEnv', {
-      environmentName: 'MyApp-env',
-      applicationName: ebApp.applicationName!,
-      solutionStackName: '64bit Amazon Linux 2 v5.9.12 running Node.js 18'
-    });
-
-    // Ensure the environment is created after the application
-    ebEnv.addDependency(ebApp);
-
-    // Step 4: Create a CloudFront Distribution
-    const cfDistribution = new cloudfront.Distribution(this, 'MyCloudFront', {
-      defaultBehavior: {
-        origin: new origins.HttpOrigin(`${ebEnv.attrEndpointUrl}`), // Correct way to reference Beanstalk URL
-        cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
-      }
-    });
-
     // Outputs
     new cdk.CfnOutput(this, 'BucketName', { value: myBucket.bucketName });
-    new cdk.CfnOutput(this, 'ElasticBeanstalkEnv', { value: ebEnv.ref });
-    new cdk.CfnOutput(this, 'CloudFrontURL', { value: cfDistribution.distributionDomainName });
   }
 }
 
