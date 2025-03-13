df63d8339fb08d53ad0f3f7e3c7d26e9c6a02acb
Aakib khan <101191261+Aakibgithuber@users.noreply.github.com>
2025-03-13 10:41:24 +0530

    Update eb-config.ts

--- lib/eb-config.ts
+++ lib/eb-config.ts
@@ -6,16 +6,31 @@ export const elasticBeanstalkConfig = [
     value: 'MyInstanceProfile' // Instance profile reference will be dynamically assigned
   },
 
-  // Environment Variables (Empty values for now)
+  // Environment Variables (Only keys, values left empty intentionally)
   ...[
     'APP_NAME',
     'AWS_REGION',
+    'DB_DATABASE',
     'DB_HOST',
-    'DB_PORT'
+    'DB_PORT',
+    'DB_USER',
+    'DB_PASSWORD',
+    'ENVIRONMENT',
+    'KC_ADMIN_CLIENT_ID',
+    'KC_ADMIN_CLIENT_SECRET',
+    'KC_BASE_URL',
+    'KC_CLIENT_ID',
+    'KC_CLIENT_SECRET'
+    'KC_CLIENT_UUID'
+    'KC_REALM'
+    'NODE_ENV'
+    'PORT'
+    'SENDGRID_API_KEY'
+    'SENDGRID_SENDER_EMAIL_ID'
   ].map((optionName) => ({
     namespace: 'aws:elasticbeanstalk:application:environment',
     optionName,
-    value: optionName === 'DB_PORT' ? '5432' : ''  // Default value for DB_PORT
+    value: ''  // Empty value for now (we'll set these in console later)
   })),
 
   // EC2 Instance Configuration
