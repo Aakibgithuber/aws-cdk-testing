8785d509acd280e308e3e7f4f34a3e8285921dc5
Aakib khan <101191261+Aakibgithuber@users.noreply.github.com>
2025-03-13 10:03:28 +0530

    Update deploy-stack.ts

--- lib/deploy-stack.ts
+++ lib/deploy-stack.ts
@@ -4,6 +4,7 @@ import * as elasticbeanstalk from 'aws-cdk-lib/aws-elasticbeanstalk';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import { elasticBeanstalkConfig } from './eb-config';  // ✅ Config Imported
 
 export class MyAwsSetupStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -25,7 +26,7 @@ export class MyAwsSetupStack extends cdk.Stack {
       assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
       managedPolicies: [
         iam.ManagedPolicy.fromAwsManagedPolicyName('AWSElasticBeanstalkWebTier'),
-        iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonS3FullAccess') // Optional, as per your needs
+        iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonS3FullAccess')
       ]
     });
 
@@ -33,18 +34,15 @@ export class MyAwsSetupStack extends cdk.Stack {
       roles: [instanceRole.roleName]
     });
 
-    // Step 4: Elastic Beanstalk Environment with Instance Profile
+    // Step 4: Elastic Beanstalk Environment
     const ebEnv = new elasticbeanstalk.CfnEnvironment(this, 'MyElasticBeanstalkEnv', {
       environmentName: 'MyApp-env',
       applicationName: ebApp.applicationName!,
-      solutionStackName: '64bit Amazon Linux 2 v5.9.12 running Node.js 18',
-      optionSettings: [
-        {
-          namespace: 'aws:autoscaling:launchconfiguration',
-          optionName: 'IamInstanceProfile',
-          value: instanceProfile.ref
-        }
-      ]
+      solutionStackName: 'Docker running on 64bit Amazon Linux 2023/4.4.4',
+      optionSettings: elasticBeanstalkConfig.map(option => ({
+        ...option,
+        value: option.optionName === 'IamInstanceProfile' ? instanceProfile.ref : option.value
+      }))
     });
 
     // Step 5: CloudFront Distribution for Beanstalk
